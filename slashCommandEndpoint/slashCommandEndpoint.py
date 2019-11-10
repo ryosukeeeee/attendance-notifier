683d74a7633580e5e39a1f99c7c44acde69a9e87
ryosukeeeee <ryou.fm.bros@gmail.com>
2019-11-10 17:37:37 +0900

    [Add] lambdaを2つに分けた。/todayに対応した

---
+++ slashCommandEndpoint/slashCommandEndpoint.py
@@ -0,0 +1,58 @@
+import boto3
+import urllib.parse
+import os
+import json
+
+def main(event, context):
+
+    # リクエストが正当か確認する
+    verification_token = os.environ['VERIFICATION_TOKEN']
+    body = map_to_dict(event["body"])
+    print(event)
+    print(body.keys())
+    if body.get("token", "") != verification_token:
+        print("token is invalid")
+
+        return {
+            'statusCode': 200,
+            'body': "",
+            'isBase64Encoded': False
+        }
+
+    # スラッシュコマンド「/today」だったらlambdaを呼ぶ
+    if body.get("command") == '/today':
+        print('slash command')
+        try:
+            client = boto3.client('lambda')
+            response = client.invoke(
+                FunctionName = os.environ['MAIN_FUNCTION_ARN'],
+                InvocationType = 'Event',
+                Payload = json.dumps(body)
+            )
+            print(response)
+        except:
+            import traceback
+            print("[Error]")
+            traceback.print_exc()
+
+        return {
+            'statusCode': 200,
+            'body': "",
+            'isBase64Encoded': False
+        }
+
+    return {
+        'statusCode': 200,
+        'body': "",
+        'isBase64Encoded': False
+    }
+
+# スラッシュコマンドのbodyをurldecodeして辞書にする
+def map_to_dict(data):
+    param = {}
+
+    for item in data.split('&'):
+        key, value = item.split('=')
+        param[key] = urllib.parse.unquote(value)
+
+    return param
